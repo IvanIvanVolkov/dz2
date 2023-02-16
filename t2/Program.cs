9260ac9f14a52646570469e69119e76c1f4ac01c
Ivan <off_2020@mail.ru>
2023-02-16 19:48:51 +0500

    t2

---
+++ t2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int anyNamber = Convert.ToInt32(Console.ReadLine());
+string anyNamberText = Convert.ToString(anyNamber);
+if (anyNamberText.Length >2){
+    Console.WriteLine("третья цифра ->"+ anyNamberText[2]);
+}
+else{
+    Console.WriteLine("-> третьей цыфры нет");
+}
